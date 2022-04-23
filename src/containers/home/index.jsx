18e560aa898c169fb1059a527f50086e2eaceeb6
sangammukherjee <sangamukherjee@gmail.com>
2022-04-23 10:26:15 +0530

    updated

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,9 +1,10 @@
 import { Animate } from "react-simple-animate";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./styles.scss";
-
+import MyPdf from "../../resumeSample/Resume_Updated.pdf";
 function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <section className="home" id="home">
       <div className="home__text-wrapper">
@@ -21,8 +22,20 @@ function Home() {
           }}
           end={{ transform: "translateX(0px)" }}
         >
-          {" "}
-          <button onClick={()=>navigate('/contact')}>Hire Me</button>
+          <div className="contact-me">
+            <div className="contact-me__buttons-wrapper">
+              <button onClick={() => navigate("/contact")}>Hire Me</button>
+              <a href={MyPdf} download="My_File.pdf">
+                Download Resume
+              </a>
+            </div>
+            <div className="contact-me__socials-wrapper">
+              <FaLinkedin size={32} />
+              <FaFacebook size={32} />
+              <FaTwitter size={32} />
+              <FaInstagram size={32} />
+            </div>
+          </div>
         </Animate>
       </div>
     </section>
